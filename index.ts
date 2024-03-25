78921060885e2c50498fdc4265280540dc38a01a
Zahira Khan <zahrakhan3567@gmail.com>
2024-03-25 07:41:15 -0700

    installed chalk to the code

--- index.ts
+++ index.ts
@@ -1,31 +1,32 @@
 
 import inquirer from "inquirer";
+import chalk from "chalk";
 
 async function calc() {
   let { weightInKgs, heightInMeters } = await inquirer.prompt([
     {
       type: "input",
       name: "weightInKgs",
-      Message: "Enter your Weight in KGs:",
+      Message: chalk.blue("Enter your Weight in KGs:"),
     },
     {
       type: "input",
       name: "heightInMeters",
-      Message: "Enter your Height in Meters:",
+      Message: chalk.blue("Enter your Height in Meters:"),
     },
   ]);
   const bodyMassIndex = weightInKgs / (heightInMeters * heightInMeters);
   console.log(bodyMassIndex);
   if (bodyMassIndex <= 18.5) {
-    console.log(
+    console.log(chalk.green(
       `your BMI Is: ${bodyMassIndex}. Congratulations! You are Under Weight.`
-    );
+    ));
   } else if (bodyMassIndex <= 25) {
-    console.log(`your BMI Is: ${bodyMassIndex}. You are Normal.`);
+    console.log(chalk.yellow(`your BMI Is: ${bodyMassIndex}. You are Normal.`));
   } else if (bodyMassIndex <= 30) {
-    console.log(`your BMI Is: ${bodyMassIndex}. Sorry! You are Over Weight.`);
+    console.log(chalk.red(`your BMI Is: ${bodyMassIndex}. Sorry! You are Over Weight.`));
   } else {
-    console.log("Don't mind You are Hugely Obese.");
+    console.log(chalk.bgRed("Don't mind You are Hugely Obese."));
   }
 }
 calc();
